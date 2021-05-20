1560f5ee3c0ec7d9143a14f98ab27598f2c5c231
WangDaFu <xiangbingze@wenru.wang>
2021-05-20 11:54:59 +0800

    init

---
+++ test/Test.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Description
+ * Date 2021/5/20 11:50 上午
+ *
+ * @author dafu
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        List<String> string = new ArrayList<>();
+        string.add("1");
+        string.add("1");
+        string.add("1");
+        string.add("1");
+    }
+}
